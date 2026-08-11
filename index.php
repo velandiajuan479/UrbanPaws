6c71f6ee3e69850092a80201f43adc41fafbc250
Juan Velandia <141973603+velandiajuan479@users.noreply.github.com>
2026-08-11 09:24:21 -0500

    se añadieron todos los enlaces para redireccionar a una vista especifica

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
     elseif ($pg==2) include 'views/vconfiguracion.php';
     elseif ($pg==3) include 'views/vcofmod.php';
     elseif ($pg==4) include 'views/vpagina.php';
+    
   ?>
 
 
--- views/vmen.php
+++ views/vmen.php
@@ -1,7 +1,35 @@
 <nav>
     <ul>
-        <li> <a href="index.php?pg=1">Mascotas</a></li>
-        <li> <a href="index.php?pg=2">Configuración</a></li>
-        <li> <a href="index.php?pg=3">Editar Configuración</a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
     </ul>
 </nav>
