b54dc990591d5b561bc4866750ef86533d83eca1
Herminson-DP <herminsondelgado6@gmail.com>
2026-08-12 10:20:35 -0500

    modified:   views/vextini.php

--- views/vextini.php
+++ views/vextini.php
@@ -36,7 +36,7 @@
                             ?>
                         </div>
 
-                        <button type="submit" class="btn btn-institutional btn-sm mb-2 d-block mx-auto" style="width: 100px; background-color: #ff7f00; color: #ffffff; border-color: #ff7f00;">
+                        <button type="submit" class="btn btn-institutional btn-sm mb-2 d-block mx-auto" style="background-color: #ff7f00; color: #ffffff; border-color: #ff7f00;">
                                 <i class="fa-solid fa-circle-user"></i> Ingresar
                             </button>
 
