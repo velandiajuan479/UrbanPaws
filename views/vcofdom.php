e7bf5ef9608b00750b486c49cd6ba0c833665a15
Juan Velandia <141973603+velandiajuan479@users.noreply.github.com>
2026-08-12 16:21:09 -0500

    creación de vista de dominio

---
+++ views/vcofdom.php
@@ -0,0 +1,85 @@
+<div class="card card-form p-4">
+    <!-- SECCIÓN 1: FORMULARIO DE REGISTRO -->
+    <h4 class="section-title">
+        <i class="fa-solid fa-box-open"></i>
+        Nuevo Dominio
+    </h4> 
+    
+    <form action="" method="POST" class="row g-3">
+    <!--Nombre del módulo-->
+        <div class="col-md-4">
+            <i class="fa-solid fa-hashtag"></i>
+            <label for="" class="form-label">Nombre del dominio</label>
+            <input type="text" class="form-control" placeholder="Tipo de documento" required>
+        </div>
+    <!--Estado del Módulo-->
+        <div class="col-md-4">
+            <i class="fa-solid fa-toggle-on"></i>
+            <label for="" class="form-label">Estado</label>
+            <div class="w-100"></div>
+            <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="estado" id="actv" value="" required>
+            <label class="form-check-label" for="actv">Activo</label>
+            </div>
+            <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="estado" id="inactv" value="" required>
+            <label class="form-check-label" for="inactv">Inactivo</label>
+            </div>
+        </div>
+    <!--Botones-->
+        <div class="col-md-12 mt-4 text-end">
+            <button type="submit" class="btn btn-primary">
+                <i class="fa-solid fa-floppy-disk fa-xl"></i>
+            </button>
+            <button type="reset" class="btn btn-accent">
+                <i class="fa-solid fa-trash fa-xl"></i>
+            </button>
+        </div>
+    </form>
+</div>
+
+<div class="table-container mt-4">
+    <h5 class="mb-3">
+        <i class="fa-solid fa-table-list"></i>
+        Lista de dominios
+    </h5>
+    <div class="table-responsive">
+        <table id="mitabla" class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Dominio</th>
+                    <th>Estado</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <strong>
+                            <i class="fa fa-box"></i>
+                            1 Módulo
+                            <br>
+                        </strong>
+                    </td>
+                    <td>Activo</td>
+                    <td>
+                        <a href="" title="editar">
+                            <i class="fa-solid fa-pencil fa-2x"></i>
+                        </a>
+                        <a href="" title="borrar">
+                            <i class="fa-solid fa-trash-can fa-2x"></i>
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th>Dominio</th>
+                    <th>Estado</th>
+                    <th></th>
+                </tr>
+            </tfoot>
+        </table>
+
+    </div>
+</div>
