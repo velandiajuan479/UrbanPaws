c5b222bc694bf5c3fc7ee0c807c12f5dfd2ecde9
Herminson-DP <herminsondelgado6@gmail.com>
2026-08-12 10:08:53 -0500

    modified:   views/vextregis.php

--- views/vextregis.php
+++ views/vextregis.php
@@ -10,17 +10,17 @@
                     <form>
 
                         <div class="col-12 mb-3">
-                            <label class="form-label fw-semibold">Nombre<span class="required-mark"></span></label>
+                            <label class="form-label fw-semibold">Nombre</label>
                             <input type="text" class="form-control" id="nombre" required placeholder="Nombre">
                         </div>
 
                         <div class="col-12 mb-3">
-                            <label class="form-label fw-semibold">Apellido<span class="required-mark"></span></label>
+                            <label class="form-label fw-semibold">Apellido</label>
                             <input type="text" class="form-control" id="apellido" required placeholder="Apellido">
                         </div>
 
                         <div class="col-12 mb-3">
-                            <label class="form-label fw-semibold">Correo Electrónico<span class="required-mark"></span></label>
+                            <label class="form-label fw-semibold">Correo Electrónico</label>
                             <input type="email" class="form-control" id="correo" required placeholder="correo@ejemplo.com">
                         </div>
 
@@ -28,9 +28,7 @@
                             <label class="form-label fw-semibold">Nueva Contraseña<span class="required-mark"></span></label>
                             <div class="input-group">
                                 <input type="password" class="form-control" id="clave" required placeholder="Mínimo 8 caracteres" minlength="8">
-                                <button class="btn btn-outline-secondary" type="button" id="togglePassword">
-                                    <i class="fa-solid fa-eye"></i>
-                                </button>
+                                <button class="btn btn-outline-secondary" type="button" id="togglePassword">Mostrar</button>
                             </div>
                         </div>
 
@@ -38,9 +36,7 @@
                             <label class="form-label fw-semibold">Confirmar Contraseña<span class="required-mark"></span></label>
                             <div class="input-group">
                                 <input type="password" class="form-control" id="clave2" required placeholder="Repite tu contraseña">
-                                <button class="btn btn-outline-secondary" type="button" id="togglePassword2">
-                                    <i class="fa-solid fa-eye"></i>
-                                </button>
+                                <button class="btn btn-outline-secondary" type="button" id="togglePassword2">Mostrar</button>
                             </div>
                         </div>
 
