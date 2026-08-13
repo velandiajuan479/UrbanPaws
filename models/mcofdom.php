65d0d8fdec9599d7b268c45ac2486dd9cb346246
Juan Velandia <141973603+velandiajuan479@users.noreply.github.com>
2026-08-13 18:41:42 -0500

    se añadieron estados en la vista y es funcional

--- models/mcofdom.php
+++ models/mcofdom.php
@@ -4,16 +4,19 @@ class mCofdom{
 
     private $iddom;
     private $nomdom;
+    private $actdom;
 
     public function getIddom(){ return $this->iddom;}
     public function getNomdom(){ return $this->nomdom;}
+    public function getActdom(){ return $this->actdom;}
 
     public function setIddom($iddom){ $this->iddom = $iddom;}
     public function setNomdom($nomdom){ $this->nomdom = $nomdom;}
+    public function setActdom($actdom){ $this->actdom = $actdom;}
 
     public function getAll(){
     try{
-        $sql = "SELECT iddom, nomdom FROM dominio";
+        $sql = "SELECT iddom, nomdom, actmod FROM dominio";
         $modelo = new Conexion();
         $conexion = $modelo->get_conexion();
         $result = $conexion->prepare($sql);
@@ -27,7 +30,7 @@ class mCofdom{
     // CONSULTAR UN REGISTRO
     public function getOne(){
     try{
-        $sql = "SELECT iddom, nomdom FROM dominio WHERE iddom=:iddom";
+        $sql = "SELECT iddom, nomdom, actdom FROM dominio WHERE iddom=:iddom";
         $modelo = new Conexion();
         $conexion = $modelo->get_conexion();
         $result = $conexion->prepare($sql);
@@ -43,8 +46,8 @@ class mCofdom{
     // GUARDAR
     public function save(){
     try{
-        $sql = "INSERT INTO dominio(nomdom)
-                VALUES(:nomdom)";
+        $sql = "INSERT INTO dominio(nomdom, actdom)
+                VALUES(:nomdom, :actdom)";
 
         $modelo = new Conexion();
         $conexion = $modelo->get_conexion();
@@ -52,8 +55,9 @@ class mCofdom{
         $result = $conexion->prepare($sql);
 
         $nomdom = $this->getNomdom();
-
+        $actdom = $this->getActdom();
         $result->bindParam(":nomdom",$nomdom);
+        $result->bindParam(":actdom", $actdom);
 
         return $result->execute();
     }catch(Exception $e){
@@ -65,7 +69,8 @@ class mCofdom{
     public function upd(){
     try{
         $sql = "UPDATE dominio
-                SET nomdom = :nomdom
+                SET nomdom = :nomdom,
+                actmod = :actmod
                 WHERE iddom = :iddom";
 
         $modelo = new Conexion();
@@ -78,6 +83,7 @@ class mCofdom{
 
         $result->bindParam(":iddom",$iddom);
         $result->bindParam(":nomdom",$nomdom);
+        $result->bindParam(":actmod",$actdom);
 
         return $result->execute();
     }catch(Exception $e){
